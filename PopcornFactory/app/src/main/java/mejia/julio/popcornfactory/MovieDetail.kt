e832fc34dbae2d1ba7998676fb78e0c7f5d2236c
Juliomst <juliosotomayor121@gmail.com>
2024-06-13 17:47:18 -0700

    Practica 6 Parte 2

---
+++ PopcornFactory/app/src/main/java/mejia/julio/popcornfactory/MovieDetail.kt
@@ -0,0 +1,15 @@
+package mejia.julio.popcornfactory
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MovieDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_detail)
+
+    }
+}
